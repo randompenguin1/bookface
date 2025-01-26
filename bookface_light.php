8a861e76181cf02185c0291da2722db47a8f5b59
Random Penguin <25882519-randompenguin@users.noreply.gitlab.com>
2025-01-26 23:08:14 +0000

    Adjust menu background color Increment version

--- bookface_light.php
+++ bookface_light.php
@@ -10,7 +10,7 @@
  * Author: Kristi H. @kmh@friendica.world feb @feb@loma.ml
  * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
  * Accented: Yes
- * Version: 1.1
+ * Version: 1.2
  */
 
 require_once 'view/theme/frio/php/PHPColors/Color.php';
@@ -20,16 +20,15 @@ $accentColor = new Color($scheme_accent);
 $nav_bg = '#ffffff';
 $nav_icon_color = '#606637';
 $link_color = '#' . $accentColor->lighten(10);
-	// overrid ugly blue accent color
+	// override ugly blue accent color
 	if ( $link_color == "#33a2e0" ){
 		 $link_color = "#0066ff";
 	}
 $background_color = '#f2f4f7';
 $background_image = '';
 $contentbg_transp = 100;
-$menu_background_hover_color = '#8080801a'; // rgba(128,128,128,.1)
+$menu_background_hover_color = '#' . $accentColor->lighten(45);
 $font_color = '#313131';
 $font_color_darker = '#333';
-$border_color = '#eee';
 
 
