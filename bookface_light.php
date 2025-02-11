15b587cdf4e433e20539fccbea1c8813d300df7c
Random Penguin <25882519-randompenguin@users.noreply.gitlab.com>
2025-02-11 21:32:01 +0000

    Replace bookface_light.php v1.3

--- bookface_light.php
+++ bookface_light.php
@@ -7,27 +7,30 @@
  *
  * Name: Bookface Light
  * Licence: AGPL
- * Author: Kristi H. @kmh@friendica.world feb @feb@loma.ml
+ * Author: Kristi H. @kmh@friendica.world feb @feb@loma.ml Phil phil@loma.ml
  * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
- * Accented: Yes
- * Version: 1.2
+ * Accented: yes
+ * Version: 1.3
  */
-
+use Friendica\DI;
 require_once 'view/theme/frio/php/PHPColors/Color.php';
 
 $accentColor = new Color($scheme_accent);
-
+$customColor = DI::pConfig()->get($uid, 'frio', 'link_color') ?: '';
+if ($customColor){
+	$customColor = new Color(''.$customColor.'');
+}
+$menu_background_hover_color = ($customColor) ? '#'.$customColor->lighten(45) : '#'.$accentColor->lighten(45);
 $nav_bg = '#ffffff';
-$nav_icon_color = '#606637';
-$link_color = '#' . $accentColor->lighten(10);
-	// override ugly blue accent color
-	if ( $link_color == "#33a2e0" ){
+$background_color = '#f2f4f7';
+$link_color = ($customColor) ? '#'.$customColor->getHex() : '#'.$accentColor->lighten(10);
+	// override ugly blue accent color and prevent setting accent to nav or bg color
+	if ( $link_color == "#33a2e0" || $link_color == $nav_bg || $link_color == $background_color ){
 		 $link_color = "#0066ff";
 	}
-$background_color = '#f2f4f7';
+$nav_icon_color = '#65686C';
 $background_image = '';
 $contentbg_transp = 100;
-$menu_background_hover_color = '#' . $accentColor->lighten(45);
 $font_color = '#313131';
 $font_color_darker = '#333';
 
