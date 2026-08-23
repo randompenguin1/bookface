41a0762e92320f9c82cc7ee57fb2407bb2abc572
Random Penguin <25882519-randompenguin@users.noreply.gitlab.com>
2026-08-23 16:57:04 -0500

    bumped version number

--- bookface_dark.php
+++ bookface_dark.php
@@ -10,6 +10,6 @@
  * Author: Pygoscelis Papua @randompenguin@friendica.world feb @feb@loma.ml Phil @phil@loma.ml
  * Overwrites: nav_bg, nav_icon_color, background_color, background_image, contentbg_transp
  * Accented: yes
- * Version: 2.1.7
+ * Version: 2.1.8
  */
 include 'bookface_assets/bookface_main.php';
