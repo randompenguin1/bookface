1bb596270c70bb1c59d335ffb09f8a64a89e00f7
Random Penguin <25882519-randompenguin@users.noreply.gitlab.com>
2026-08-23 17:47:49 -0500

    added better handling of accent colors for dark mode

--- bookface_assets/bookface_main.php
+++ bookface_assets/bookface_main.php
@@ -8,6 +8,10 @@ $customColor = DI::pConfig()->get($uid, 'frio', 'link_color') ?: '';
 if ($customColor){
 	$customColor = new Color(''.$customColor.'');
 }
+
+$bookfaceBlue = new Color('#0066ff');
+$bookfaceGreen = new Color('#008000');
+
 $menu_background_hover_light = ($customColor) ? '#'.$customColor->lighten(45) : '#'.$accentColor->lighten(45);
 $menu_background_hover_dark  = ($customColor) ? '#'.$customColor->darken(20) : '#'.$accentColor->darken(20);
 $menu_background_hover_color = 'light-dark('.$menu_background_hover_light.', '.$menu_background_hover_dark.')';
@@ -16,15 +20,35 @@ $nav_bg = 'light-dark(#ffffff, #252728)';
 $background_color = 'light-dark(#f2f4f7, #1C1C1D)';
 
 $link_color_light = ($customColor) ? '#'.$customColor->getHex() : '#'.$accentColor->getHex();
+$link_color_dark  = ($customColor) ? '#'.$customColor->getHex() : '#'.$accentColor->lighten(25);
 	// override ugly blue accent color and prevent setting accent to nav, bg color, or white
-	if ( $link_color_light == "#1e87c2" || $link_color_light == $nav_bg || $link_color_light == $background_color || $link_color_light == '#ffffff' ){
+	if ($link_color_light == $nav_bg || $link_color_light == $background_color || $link_color_light == "#ffffff"){
 		 $link_color_light = "#0066ff";
 	}
-$link_color_dark  = ($customColor) ? '#'.$customColor->getHex() : '#'.$accentColor->getHex();
-	// override ugly blue accent color and prevent setting accent to nav, bg color, or black
-	if ( $link_color_dark == "#1e87c2" || $link_color_dark == $nav_bg || $link_color_dark == $background_color || $link_color_dark == '#000000' ){
-		 $link_color_dark = "#0066ff";
+	/* pure gray can mess up contrast color for text */
+	if ( $link_color_light == "#808080" ){
+		 $link_color_light = "#737373";
 	}
+	// override ugly blue accent color and prevent setting accent to nav, bg color, or white
+	if ($link_color_dark == $nav_bg || $link_color_dark == $background_color || $link_color_dark == "#000000"){
+		 $link_color = "#0066ff";
+	}
+	/* pure gray can mess up contrast color for text */
+	if ( $link_color_dark == "#808080" ){
+		 $link_color_dark = "#737373";
+	}
+switch ($scheme_accent) {
+	case FRIO_SCHEME_ACCENT_BLUE:
+		$link_color_light = '#0066ff';
+		$link_color_dark  = '#'.$bookfaceBlue->lighten(10);
+		break;
+	case FRIO_SCHEME_ACCENT_GREEN:
+		$link_color_light =  '#008000';
+		$link_color_dark  =  '#'.$accentColor->lighten(10);
+		break;
+	default:
+		// change nothing
+}
 $link_color = 'light-dark('.$link_color_light.', '.$link_color_dark.')';
 $link_hover_color = $link_color;
 
